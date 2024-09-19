cc80e25e20e6d840326fad95f1902bccb6d99ff1
Nghia-Truong <chuon.chongia1306@gmail.com>
2024-09-19 22:20:55 +0900

    update customer

--- store/controller/main.js
+++ store/controller/main.js
@@ -1,1 +1,17 @@
-//
+import { renderListProducts } from "./controller.js"
+
+const BASE_URL = "https://66d121c662816af9a4f2d21e.mockapi.io/api/v1/product";
+
+var products = {};
+let fetchData = async () => {
+    let res = await axios({
+        url: BASE_URL,
+        method: "GET",
+    });
+    products = res.data
+    renderListProducts(products)
+    return products
+}
+console.log("🚀 [ products:", products)
+
+fetchData()
